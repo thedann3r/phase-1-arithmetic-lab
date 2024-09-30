7133d1475d9f62b60d41a255d7a4f8fb2def259e
dann <austindan168@gmail.com>
2024-09-30 18:57:34 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1
+console.log(random)
+
+let num3 = 8
+let num4 = 2
+let mod = num3 / num4
+console.log(mod)
+
+let max = (2, 3, 14, 20)
+Math.max(max)
+console.log(max)
